291dcf7c62c006b1d4f1efaa1435c280e8d96ece
wgentertainmentgroup-star <wg.entertainmentgroup@gmail.com>
2026-08-09 22:17:48 -0400

    Restore Android health-check assertion import

--- android/app/src/androidTest/java/com/bigfootsoftware/bigfootsday/MainActivityRegressionTest.java
+++ android/app/src/androidTest/java/com/bigfootsoftware/bigfootsday/MainActivityRegressionTest.java
@@ -1,6 +1,7 @@
 package com.bigfootsoftware.bigfootsday;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import android.Manifest;
